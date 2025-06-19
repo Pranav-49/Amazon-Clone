41619b3740692d04ebda44d8c899fa0eb0ec188a
Pranav-49 <pranavraychure@gmail.com>
2025-06-19 19:55:27 +0530

    Fixed Radio Selector

--- checkout.js
+++ checkout.js
@@ -26,14 +26,14 @@ cart.forEach((cartItem) => {
 
         <div class="cart-item-details-grid">
         <img class="product-image"
-            src="images/products/athletic-cotton-socks-6-pairs.jpg">
+            src="${matchingProduct.image}">
 
         <div class="cart-item-details">
             <div class="product-name">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
+            ${matchingProduct.name}
             </div>
             <div class="product-price">
-            $10.90
+            $${(matchingProduct.priceCents /100).toFixed(2)}
             </div>
             <div class="product-quantity">
             <span>
@@ -55,7 +55,7 @@ cart.forEach((cartItem) => {
             <div class="delivery-option">
             <input type="radio" checked
                 class="delivery-option-input"
-                name="delivery-option-1">
+                name="delivery-option-${matchingProduct.id}">
             <div>
                 <div class="delivery-option-date">
                 Tuesday, June 21
@@ -68,7 +68,7 @@ cart.forEach((cartItem) => {
             <div class="delivery-option">
             <input type="radio"
                 class="delivery-option-input"
-                name="delivery-option-1">
+                name="delivery-option-${matchingProduct.id}">
             <div>
                 <div class="delivery-option-date">
                 Wednesday, June 15
@@ -81,7 +81,7 @@ cart.forEach((cartItem) => {
             <div class="delivery-option">
             <input type="radio"
                 class="delivery-option-input"
-                name="delivery-option-1">
+                name="delivery-option-${matchingProduct.id}">
             <div>
                 <div class="delivery-option-date">
                 Monday, June 13
